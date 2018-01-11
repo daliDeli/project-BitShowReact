15a1a0c94e8ab6cc745d8c3214445f4fb659f383
daliDeli <bruns8900@gmail.com>
2018-01-11 18:21:49 +0100

    Updates

--- src/entities/Series.js
+++
@@ -1,8 +0,0 @@
-export default class Series  {
-    constructor(series) {
-        this.image = series.image.medium;
-        this.name = series.name;
-        this.id = series.id;
-       
-    }
-}
