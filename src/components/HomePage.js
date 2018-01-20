0ecf7baf8d5790e2d6ff924fe5ef2db4a96fc010
daliDeli <bruns8900@gmail.com>
2018-01-20 23:44:48 +0100

    working on bugs

---
+++ src/components/HomePage.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+import DataService from "../services/DataService";
+import ShowCard from "./ShowCard";
+import "./HomePage.css";
+
+
+export default class HomePage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            series: []
+        }
+
+        this.dataService = new DataService();
+
+        this.getSeries = this.getSeries.bind(this);
+        this.successSeriesHandler = this.successSeriesHandler.bind(this);
+
+    }
+
+    getSeries() {
+        this.dataService.getAllSeries(this.successSeriesHandler, this.failedRequest);
+    }
+
+    successSeriesHandler(series) {
+        console.log("primio", series);
+        this.setState({
+            series
+        })
+    }
+
+    componentDidMount() {
+        this.getSeries();
+    }
+
+    failedRequest(error) {
+        console.log(error)
+    }
+
+    render() {
+        return (
+            <main>
+                <div className="container-fluid">
+                    <div className="row">
+
+                        {this.state.series.map(series =>
+                            <Link to={`/single/${series.id}`} className="col-12 col-md-6 col-lg-4 text-center top-buffer" key={series.id}>
+                                <ShowCard image={series.image} name={series.name} key={series.id} />
+                            </Link>
+                        )}
+                    </div>
+                </div>
+            </main>
+        );
+    }
+}
---
+++ src/entities/Series.js
@@ -0,0 +1,8 @@
+export default class Series  {
+    constructor(series) {
+        this.image = series.image.medium;
+        this.name = series.name;
+        this.id = series.id;
+       
+    }
+}
